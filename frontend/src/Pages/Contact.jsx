02f0a5e5eca675dfd28518f5e047cfb4bd8c3d48
Soul-Scribes <168422977+Soul-Scribes@users.noreply.github.com>
2024-07-21 21:01:17 +0100

    added personal images to home page and contact details

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaPhone, FaEnvelope, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaPhone, FaEnvelope, FaInstagram, FaTwitter, FaTiktok } from 'react-icons/fa';
 import '../App.css';
 
 const Contact = () => {
@@ -7,7 +7,7 @@ const Contact = () => {
     <div className="contact-container">
       <h2 className="contact-title">Contact Us</h2>
       <div className="contact-icons">
-        <a href="tel:+1234567890" aria-label="Phone" className="contact-icon">
+        <a href="tel:+44 7964543278" aria-label="Phone" className="contact-icon">
           <FaPhone />
           <span>+44 7964543278</span>
         </a>
@@ -22,6 +22,10 @@ const Contact = () => {
         <a href="https://x.com/picklehill" aria-label="X.com" target="_blank" rel="noopener noreferrer" className="contact-icon">
           <FaTwitter />
           <span>@picklehill</span>
+          </a>
+        <a href="https://www.tiktok.com/@picklehillltd?_t=8oDQDpnaGKu&_r=1" aria-label="tiktok.com" target="_blank" rel="noopener noreferrer" className="contact-icon">
+          <FaTiktok />
+          <span>@picklehillLtd</span>
         </a>
       </div>
     </div>
