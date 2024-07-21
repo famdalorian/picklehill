adcd86931e01ce97e3e889360724e72629875e16
Soul-Scribes <168422977+Soul-Scribes@users.noreply.github.com>
2024-07-21 21:42:27 +0100

    added video

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,6 +4,18 @@ const Home = () => {
   return (
     <div className="content">
       <h1>Welcome to PickleHill Ltd</h1>
+
+      <div className="video-container">
+        <iframe 
+          title="vimeo-player" 
+          src="https://player.vimeo.com/video/988106334?h=9544e92f38" 
+          width="640" 
+          height="360" 
+          frameBorder="0" 
+          allowFullScreen
+        ></iframe>
+      </div>
+      
       <p>We specialize in crafting exquisite cakes for all occasions.</p>
       <p>
         From elegant wedding cakes to whimsical birthday creations, we create
